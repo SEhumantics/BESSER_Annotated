4082e0398332caa3ab86ad6f40354d9cb5d66357
ivan-alfonso <ivandaalf@hotmail.com>
2024-10-30 16:04:27 +0100

    Add the method to create a sheet for many-to-many relationships

--- besser/generators/spreadsheet/spreadsheet_generator.py
+++ besser/generators/spreadsheet/spreadsheet_generator.py
@@ -1,5 +1,5 @@
 import os
-from besser.BUML.metamodel.structural import DomainModel, Class
+from besser.BUML.metamodel.structural import DomainModel, Class, BinaryAssociation
 from besser.generators import GeneratorInterface
 from besser.utilities import sort_by_timestamp
 from openpyxl import Workbook
@@ -34,33 +34,39 @@ class SpreadSheetGenerator(GeneratorInterface):
 
     def __init__(self, model: DomainModel, output_dir: str = None):
         super().__init__(model, output_dir)
+        self.__workbook = None
 
-    def generate(self):
+    def generate(self, *args) -> None:
         """
         Main method to generate the spreadsheet. Loops through classes to create sheets and columns,
         and sets data validation for associations.
         """
-        workbook = Workbook()
-        self._create_class_sheets(workbook)
-        self._create_association_comboboxes(workbook)
+        self.__workbook = Workbook()
+
+        # Add styles to workbook to prevent re-creation issues
+        for style in (self.text_style, self.number_style, self.date_style, self.time_style, self.datetime_style):
+            if style.name not in self.__workbook.named_styles:
+                self.__workbook.add_named_style(style)
 
         # Remove default sheet created by openpyxl
-        default_sheet = workbook["Sheet"]
-        workbook.remove(default_sheet)
+        default_sheet = self.__workbook["Sheet"]
+        self.__workbook.remove(default_sheet)
+
+        self._create_class_sheets()
+        self._create_association_comboboxes()
+        self._create_bridge_sheets()
 
         # Save file
         file_path = self.build_generation_path(file_name="model.xlsx")
-        workbook.save(file_path)
+        self.__workbook.save(file_path)
         print(f"Generated file: '{file_path}'")
 
-    def _create_class_sheets(self, workbook):
+    def _create_class_sheets(self) -> None:
         """
         Creates a sheet for each class, applies styles, and fills with example values.
         """
-        for buml_class in sort_by_timestamp(self.model.types):
-            if not isinstance(buml_class, Class):
-                continue
-            sheet = workbook.create_sheet(title=buml_class.name)
+        for buml_class in sort_by_timestamp(self.model.get_classes()):
+            sheet = self.__workbook.create_sheet(title=buml_class.name)
 
             attrs = sort_by_timestamp(buml_class.attributes)
             for column_index, attribute in enumerate(attrs, start=1):
@@ -75,17 +81,18 @@ class SpreadSheetGenerator(GeneratorInterface):
                     data_cell.style = style
                     if row == 2:  # Set example value only in the first row for example data
                         if style == self.text_style:
-                            example_value = buml_class.name + "_" + attribute.name
+                            example_value = f"{buml_class.name}_{attribute.name}"
                         data_cell.value = example_value
 
-    def _create_association_comboboxes(self, workbook):
+    def _create_association_comboboxes(self) -> None:
         """
-        Adds comboboxes to each class sheet for one-to-one associations, linked to other sheets.
+        Adds comboboxes to each class sheet for one-to-one and one-to-many associations, linked to
+        other sheets.
         """
-        for buml_class in sort_by_timestamp(self.model.types):
+        for buml_class in sort_by_timestamp(self.model.get_classes()):
             for end in sort_by_timestamp(buml_class.association_ends()):
                 if end.multiplicity.max == 1:
-                    sheet = workbook[buml_class.name]
+                    sheet = self.__workbook[buml_class.name]
                     new_column_index = sheet.max_column + 1
                     header_cell = sheet.cell(row=1, column=new_column_index, value=end.name)
                     header_cell.font = Font(bold=True)
@@ -105,5 +112,40 @@ class SpreadSheetGenerator(GeneratorInterface):
                     sheet.add_data_validation(data_validation)
 
                     # Automatically set the first value of the dropdown for the first row
-                    first_option = workbook[end.type.name].cell(row=2, column=1).value
+                    first_option = self.__workbook[end.type.name].cell(row=2, column=1).value
                     sheet.cell(row=2, column=new_column_index, value=first_option)
+
+    def _configure_association_column(self, sheet, end, column_index) -> None:
+        """Configures a column in the sheet for an association end with data validation."""
+        header_cell = sheet.cell(row=1, column=column_index, value=end.name)
+        header_cell.font = Font(bold=True)
+        formula = f"{end.type.name}!$A$2:$A$50"
+        data_validation = DataValidation(type="list", formula1=formula, showDropDown=False)
+        data_validation.error = "Select a valid option"
+        data_validation.errorTitle = "Invalid option"
+
+        for row in range(2, self.MAX_ROWS):
+            cell = sheet.cell(row=row, column=column_index)
+            data_validation.add(cell)
+        
+        sheet.add_data_validation(data_validation)
+
+        # Set default value for the first row
+        first_option = self.__workbook[end.type.name].cell(row=2, column=1).value
+        sheet.cell(row=2, column=column_index, value=first_option)
+
+    def _create_bridge_sheets(self) -> None:
+        """
+        Creates a sheet for many-to-many relationships.
+        """
+        for assoc in self.model.associations:
+            if not isinstance(assoc, BinaryAssociation):
+                continue
+            if all(end.multiplicity.max == 9999 for end in assoc.ends):
+                sheet = self.__workbook.create_sheet(title=assoc.name)
+
+                # Get the sorted ends
+                end1, end2 = sort_by_timestamp(assoc.ends)
+                # Configure each association column
+                self._configure_association_column(sheet, end1, 1)
+                self._configure_association_column(sheet, end2, 2)
